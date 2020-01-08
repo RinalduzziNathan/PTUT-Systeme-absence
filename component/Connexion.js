4b11364b2118958abaa2212f9c6777aad5cc95d5
Anaïs Thoral <anaisthoral@gmail.com>
2020-01-08 17:01:23 +0100

    Connexion
    Style

--- component/Connexion.js
+++ component/Connexion.js
@@ -1,4 +1,4 @@
-import { TextInput, View, StyleSheet, Button, Image } from "react-native";
+import { TextInput, View, StyleSheet, Button, Image, TouchableOpacity, Text} from "react-native";
 import React from 'react';
 
 class Connexion extends React.Component{
@@ -6,16 +6,19 @@ class Connexion extends React.Component{
     render(){
         return(
             <View style={styles.main_container}>
-                <Image style={styles.image} source={require('../Image/logo-uca-.png')} />
-                <TextInput style = {styles.textinput} placeholder='Identifiant'/>
-                <TextInput style = {styles.textinput} placeholder='Mot De Passe'/>
-                <Button title="Connexion" />
+                <View style={styles.container, styles.logo_container}>
+                    <Image style={styles.image} source={require('../Image/logo-uca-.png')} />
+                </View>
+                <View style={styles.container, styles.input_button_container}>
+                    <TextInput style = {styles.textinput} placeholder='Identifiant' placeholderTextColor="#FFFFFF" color='#FFFFFF'/>
+                    <TextInput style = {styles.textinput} placeholder='Mot De Passe' placeholderTextColor="#FFFFFF" color='#FFFFFF' />
+                    <TouchableOpacity style={styles.buttonStyle} activeOpacity = { .5 }>
+                        <Text style={styles.buttonText}>CONNEXION</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
-
-    //faire deux view 
-    //la grande à l'horizontal
 
 }
 
@@ -23,23 +26,48 @@ const styles = StyleSheet.create({
     main_container: {
       flex: 1,
       justifyContent : 'center',
-      backgroundColor: '#158988'
+      backgroundColor: '#38B2A8',
+      alignItems:"center"
     },
     textinput: {
       marginLeft: 5,
       marginRight: 5,
-      marginTop: 5,
+      marginTop: 20,
       height: 50,
-      borderColor: '#000000',
+      borderColor: '#0C5E7F',
       borderWidth: 1,
-      paddingLeft: 5
+      paddingLeft: 5,
+      width:300,
+      backgroundColor: '#0C5E7F'
     },
     image:{
         width: 150,
         height: 150,
-        
-        
+        marginTop: 40
+    },
+    container: {
+        justifyContent: 'center'
+    },
+    input_button_container: {
+        flex:3
+    },
+    logo_container: {
+        flex:2
+    },
+    buttonStyle:{
+        marginLeft:5,
+        marginTop:20,
+        backgroundColor:'#3FC9BE',
+        width: 300,
+        height: 50
+    },
+    buttonText:{
+        textAlign: 'center',
+        color:'#fff',
+        marginVertical: 18,
+        fontWeight: 'bold',
+        fontSize: 17
     }
-  })
+ })
 
   export default Connexion;
